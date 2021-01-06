9478ad2d63dcc4e3feb5198cad03e9f42c440f83
emmachio <echio20@stuy.edu>
2021-01-06 13:15:13 -0500

    node class

--- Node.java
+++ Node.java
@@ -1,9 +1,10 @@
 public class Node {
+  private String data;
+  private Node next, prev;
+
   public Node(String value) {
     data = value;
   }
-  private String data;
-  private Node next, prev;
 
   public Node getNext() {
     return next;
